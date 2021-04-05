86cc2657feaf74550c21c04eb8a772b0f76be7de
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-04-05 13:06:34 +0200

    Update ticket2.0.js

--- commands/ticket/ticket2.0.js
+++ commands/ticket/ticket2.0.js
@@ -22,7 +22,7 @@ class ticket extends Command {
 		
         var opmerking = args.slice(0).join(" ");
 
-        const categoryID = "825265915032633374";
+        const categoryID = "773115115615944705";
 		const staff = (message.member.roles.cache.some(role => role.name === 'Support Team'));
 
         var userName = message.author.username;
