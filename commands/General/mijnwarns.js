3ce4f1367452b0585ae8c7b915b286e310c75b17
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-04-11 14:44:57 +0200

    Create mijnwarns.js

---
+++ commands/General/mijnwarns.js
@@ -0,0 +1,48 @@
+const Command = require("../../base/Command.js"),
+	Discord = require("discord.js");
+
+class mywarns extends Command {
+
+	constructor (client) {
+		super(client, {
+			name: "mywarns",
+			dirname: __dirname,
+			enabled: true,
+			guildOnly: true,
+			aliases: [],
+			memberPermissions: [],
+			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			nsfw: false,
+			ownerOnly: false,
+			cooldown: 3000
+		});
+	}
+
+	async run (message, args, data) {
+        
+		const user = message.author 
+			
+		const memberData = await this.client.findOrCreateMember({ id: user.id, guildID: message.guild.id });
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(user.tag, user.displayAvatarURL())
+			.setColor(data.config.embed.color)
+			.setFooter(data.config.embed.footer);
+
+		if(memberData.sanctions.length < 1){
+			embed.setDescription(message.translate("moderation/sanctions:NO_SANCTION", {
+				username: user.tag
+			}));
+			return message.channel.send(embed);
+		} else {
+			memberData.sanctions.forEach((s) => {
+				embed.addField(s.type+" | #"+s.case, `${message.translate("common:MODERATOR")}: <@${s.moderator}>\n${message.translate("common:REASON")}: ${s.reason}`, true);
+			});
+		}
+
+		message.channel.send(embed);
+	}
+
+}
+
+module.exports = mywarns;
