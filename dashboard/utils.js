e6f013f7ab5ca5f57eb82c8d4f1e36baf97b92a8
raymond390 <74500294+raymond390@users.noreply.github.com>
2021-04-14 15:09:56 +0200

    s

--- dashboard/utils.js
+++ dashboard/utils.js
@@ -52,7 +52,7 @@ async function fetchUser(userData, client, query, data){
 memberData = require("../base/Member");
 
 			const memberData = (message.author === user) ? data.memberData : await this.client.findOrCreateMember({ id: user.id, guildID: message.guild.id }); 
-		  levelFile = data.memberData.level;
+	
 
 }
 
